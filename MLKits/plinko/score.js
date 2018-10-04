03574ba106666ad6d50a6b6554e1997026771e4a
stephen grider <ste.grider@gmail.com>
2018-10-04 17:56:08 -0600

    investigate k values

--- MLKits/plinko/score.js
+++ MLKits/plinko/score.js
@@ -1,26 +1,25 @@
 const outputs = [];
-const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
-  const testSetSize = 10;
+  const testSetSize = 100;
   const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
 
-  let numberCorrect = 0;
-  for (let i = 0; i < testSet.length; i++) {
-    const bucket = knn(trainingSet, testSet[i][0]);
-    if (bucket === testSet[i][3]) {
-      numberCorrect++;
-    }
-  }
+  _.range(1, 20).forEach(k => {
+    const accuracy = _.chain(testSet)
+      .filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3])
+      .size()
+      .divide(testSetSize)
+      .value();
 
-  console.log('Accuracy:', numberCorrect / testSetSize);
+    console.log('For k of', k, 'accuracy is', accuracy);
+  });
 }
 
-function knn(data, point) {
+function knn(data, point, k) {
   return _.chain(data)
     .map(row => [distance(row[0], point), row[3]])
     .sortBy(row => row[0])
