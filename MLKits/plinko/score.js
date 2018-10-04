d0b0458b6fd7e1ee29c531a7ad863d39e02ab8e3
stephen grider <ste.grider@gmail.com>
2018-10-04 17:30:43 -0600

    print report

--- MLKits/plinko/score.js
+++ MLKits/plinko/score.js
@@ -6,12 +6,18 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-  const [testSet, trainingSet] = splitDataset(outputs, 10);
+  const testSetSize = 10;
+  const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
 
+  let numberCorrect = 0;
   for (let i = 0; i < testSet.length; i++) {
     const bucket = knn(trainingSet, testSet[i][0]);
-    console.log(bucket, testSet[i][3]);
+    if (bucket === testSet[i][3]) {
+      numberCorrect++;
+    }
   }
+
+  console.log('Accuracy:', numberCorrect / testSetSize);
 }
 
 function knn(data, point) {
