6f12fd92f72eae94c36d6c3a8e4924165ed3c3ad
stephen grider <ste.grider@gmail.com>
2018-10-04 17:17:07 -0600

    generalize knn

--- MLKits/plinko/score.js
+++ MLKits/plinko/score.js
@@ -1,5 +1,4 @@
 const outputs = [];
-const predictionPoint = 300;
 const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
@@ -7,8 +6,12 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-  const bucket = _.chain(outputs)
-    .map(row => [distance(row[0]), row[3]])
+  const bucket = console.log('Your point will probably fall into', bucket);
+}
+
+function knn(data, point) {
+  return _.chain(data)
+    .map(row => [distance(row[0], point), row[3]])
     .sortBy(row => row[0])
     .slice(0, k)
     .countBy(row => row[1])
@@ -18,12 +21,10 @@ function runAnalysis() {
     .first()
     .parseInt()
     .value();
-
-  console.log('Your point will probably fall into', bucket);
 }
 
-function distance(point) {
-  return Math.abs(point - predictionPoint);
+function distance(pointA, pointB) {
+  return Math.abs(pointA - pointB);
 }
 
 function splitDataset(data, testCount) {
