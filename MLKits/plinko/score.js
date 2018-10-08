f6ae0fdf69bd7b176262c8bc71a6ecfbd887238b
stephen grider <ste.grider@gmail.com>
2018-10-08 12:23:58 -0600

    feat

--- MLKits/plinko/score.js
+++ MLKits/plinko/score.js
@@ -10,7 +10,9 @@ function runAnalysis() {
 
   _.range(1, 20).forEach(k => {
     const accuracy = _.chain(testSet)
-      .filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3])
+      .filter(
+        testPoint => knn(trainingSet, _.initial(testPoint), k) === testPoint[3]
+      )
       .size()
       .divide(testSetSize)
       .value();
@@ -21,11 +23,13 @@ function runAnalysis() {
 
 function knn(data, point, k) {
   return _.chain(data)
-    .map(row => [distance(row[0], point), row[3]])
+    .map(row => {
+      return [distance(_.initial(row), point), _.last(row)];
+    })
     .sortBy(row => row[0])
     .slice(0, k)
     .countBy(row => row[1])
-    .toPairs()_.
+    .toPairs()
     .sortBy(row => row[1])
     .last()
     .first()
@@ -34,11 +38,13 @@ function knn(data, point, k) {
 }
 
 function distance(pointA, pointB) {
-  return _.chain(pointA)
-    .zip(pointB)
-    .map(([a, b]) => (a - b) ** 2)
-    .sum()
-    .value() ** 0.5;
+  return (
+    _.chain(pointA)
+      .zip(pointB)
+      .map(([a, b]) => (a - b) ** 2)
+      .sum()
+      .value() ** 0.5
+  );
 }
 
 function splitDataset(data, testCount) {
