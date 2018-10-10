b364c05624d0590c6c089fcaaecf5a20e0042c88
stephen grider <ste.grider@gmail.com>
2018-10-10 16:12:46 -0600

    apply

--- MLKits/knn-tf/index.js
+++ MLKits/knn-tf/index.js
@@ -3,9 +3,15 @@ const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./load-csv');
 
 function knn(features, labels, predictionPoint, k) {
+  const { mean, variance } = tf.moments(features, 0);
+
+  const scaledPrediction = predictionPoint.sub(mean).div(variance.pow(0.5));
+
   return (
     features
-      .sub(predictionPoint)
+      .sub(mean)
+      .div(variance.pow(0.5))
+      .sub(scaledPrediction)
       .pow(2)
       .sum(1)
       .pow(0.5)
@@ -23,7 +29,7 @@ let { features, labels, testFeatures, testLabels } = loadCSV(
   {
     shuffle: true,
     splitTest: 10,
-    dataColumns: ['lat', 'long'],
+    dataColumns: ['lat', 'long', 'sqft_lot'],
     labelColumns: ['price']
   }
 );
