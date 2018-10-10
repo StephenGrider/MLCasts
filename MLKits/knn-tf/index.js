9f19dabb5667c09e21cbd1e8d95b804226630f41
stephen grider <ste.grider@gmail.com>
2018-10-10 15:24:00 -0600

    error

---
+++ MLKits/knn-tf/index.js
@@ -0,0 +1,38 @@
+require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs');
+const loadCSV = require('./load-csv');
+
+function knn(features, labels, predictionPoint, k) {
+  return (
+    features
+      .sub(predictionPoint)
+      .pow(2)
+      .sum(1)
+      .pow(0.5)
+      .expandDims(1)
+      .concat(labels, 1)
+      .unstack()
+      .sort((a, b) => (a.get(0) > b.get(0) ? 1 : -1))
+      .slice(0, k)
+      .reduce((acc, pair) => acc + pair.get(1), 0) / k
+  );
+}
+
+let { features, labels, testFeatures, testLabels } = loadCSV(
+  'kc_house_data.csv',
+  {
+    shuffle: true,
+    splitTest: 10,
+    dataColumns: ['lat', 'long'],
+    labelColumns: ['price']
+  }
+);
+
+features = tf.tensor(features);
+labels = tf.tensor(labels);
+
+testFeatures.forEach((testPoint, i) => {
+  const result = knn(features, labels, tf.tensor(testPoint), 10);
+  const err = (testLabels[i][0] - result) / testLabels[i][0];
+  console.log('Error', err * 100);
+});
