02df2f271000609cf923379f89d00a015a2618bd
stephen grider <ste.grider@gmail.com>
2018-10-12 14:39:16 -0600

    data

---
+++ MLKits/regressions/index.js
@@ -0,0 +1,10 @@
+require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs');
+const loadCSV = require('./load-csv');
+
+let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
+  shuffle: true,
+  splitTest: 50,
+  dataColumns: ['horsepower'],
+  labelColumns: ['mpg']
+});
