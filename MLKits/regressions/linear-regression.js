84431f13767ab9218ca81efc3744aca55c1f03eb
stephen grider <ste.grider@gmail.com>
2018-10-12 15:59:39 -0600

    slopes

--- MLKits/regressions/linear-regression.js
+++ MLKits/regressions/linear-regression.js
@@ -1,4 +1,5 @@
 const tf = require('@tensorflow/tfjs');
+const _ = require('lodash');
 
 class LinearRegression {
   constructor(features, labels, options) {
@@ -18,6 +19,23 @@ class LinearRegression {
     const currentGuessesForMPG = this.features.map(row => {
       return this.m * row[0] + this.b;
     });
+
+    const bSlope =
+      (_.sum(
+        currentGuessesForMPG.map((guess, i) => {
+          return guess - this.labels[i][0];
+        })
+      ) *
+        2) /
+      this.features.length;
+    const mSlope =
+      (_.sum(
+        currentGuessesForMPG.map((guess, i) => {
+          return -1 * this.features[i][0] * (this.labels[i][0] - guess);
+        })
+      ) *
+        2) /
+      this.features.length;
   }
 
   train() {
