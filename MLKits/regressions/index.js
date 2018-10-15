b928040812d9244946892de128abb0f1abb14fa1
stephen grider <ste.grider@gmail.com>
2018-10-15 12:54:42 -0600

    add matmul

--- MLKits/regressions/index.js
+++ MLKits/regressions/index.js
@@ -11,9 +11,8 @@ let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
 });
 
 const regression = new LinearRegression(features, labels, {
-  learningRate: 0.001,
-  iterations: 1
+  learningRate: 0.0001,
+  iterations: 100
 });
 
-regression.train();
-console.log('M value is', regression.m, 'B value is', regression.b);
+regression.gradientDescent();
--- MLKits/regressions/linear-regression.js
+++ MLKits/regressions/linear-regression.js
@@ -3,42 +3,24 @@ const _ = require('lodash');
 
 class LinearRegression {
   constructor(features, labels, options) {
-    this.features = features;
-    this.labels = labels;
+    this.features = tf.tensor(features);
+    this.labels = tf.tensor(labels);
+
+    this.features = this.features.concat(
+      tf.ones([this.features.shape[0], 1]),
+      1
+    );
 
     this.options = Object.assign(
       { learningRate: 0.1, iterations: 1000 },
       options
     );
 
-    this.m = 0;
-    this.b = 0;
+    this.weights = tf.ones([2, 1]);
   }
 
   gradientDescent() {
-    const currentGuessesForMPG = this.features.map(row => {
-      return this.m * row[0] + this.b;
-    });
-
-    const bSlope =
-      (_.sum(
-        currentGuessesForMPG.map((guess, i) => {
-          return guess - this.labels[i][0];
-        })
-      ) *
-        2) /
-      this.features.length;
-    const mSlope =
-      (_.sum(
-        currentGuessesForMPG.map((guess, i) => {
-          return -1 * this.features[i][0] * (this.labels[i][0] - guess);
-        })
-      ) *
-        2) /
-      this.features.length;
-
-    this.m = this.m - mSlope * this.options.learningRate;
-    this.b = this.b - bSlope * this.options.learningRate;
+    this.features.matMul(this.weights).print();
   }
 
   train() {
