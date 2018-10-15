bfd4ce57abca9552c18a76969f97ee1ac20b4b8d
stephen grider <ste.grider@gmail.com>
2018-10-15 17:53:02 -0600

    standardization

--- MLKits/regressions/linear-regression.js
+++ MLKits/regressions/linear-regression.js
@@ -56,7 +56,22 @@ class LinearRegression {
     features = tf.tensor(features);
     features = tf.ones([features.shape[0], 1]).concat(features, 1);
 
+    if (this.mean && this.variance) {
+      features = features.sub(this.mean).div(this.variance.pow(0.5));
+    } else {
+      features = this.standardize(features);
+    }
+
     return features;
+  }
+
+  standardize(features) {
+    const { mean, variance } = tf.moments(features, 0);
+
+    this.mean = mean;
+    this.variance = variance;
+
+    return features.sub(mean).div(variance.pow(0.5));
   }
 }
 
