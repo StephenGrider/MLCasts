6f1aa8d826dbecf7aaa861a5f9d0ae13426f7d78
stephen grider <ste.grider@gmail.com>
2018-10-15 15:24:27 -0600

    refactor

--- MLKits/regressions/linear-regression.js
+++ MLKits/regressions/linear-regression.js
@@ -3,42 +3,29 @@ const _ = require('lodash');
 
 class LinearRegression {
   constructor(features, labels, options) {
-    this.features = features;
-    this.labels = labels;
+    this.features = tf.tensor(features);
+    this.labels = tf.tensor(labels);
+
+    this.features = tf
+      .ones([this.features.shape[0], 1])
+      .concat(this.features, 1);
 
     this.options = Object.assign(
       { learningRate: 0.1, iterations: 1000 },
       options
     );
 
-    this.m = 0;
-    this.b = 0;
+    this.weights = tf.zeros([2, 1]);
   }
 
   gradientDescent() {
-    const currentGuessesForMPG = this.features.map(row => {
-      return this.m * row[0] + this.b;
-    });
+    const currentGuesses = this.features.matMul(this.weights);
+    const differences = currentGuesses.sub(this.labels);
 
-    const bSlope =
-      (_.sum(
-        currentGuessesForMPG.map((guess, i) => {
-          return guess - this.labels[i][0];
-        })
-      ) *
-        2) /
-      this.features.length;
-    const mSlope =
-      (_.sum(
-        currentGuessesForMPG.map((guess, i) => {
-          return this.features[i][0] * (guess - this.labels[i][0]);
-        })
-      ) *
-        2) /
-      this.features.length;
-
-    this.m = this.m - mSlope * this.options.learningRate;
-    this.b = this.b - bSlope * this.options.learningRate;
+    const slopes = this.features
+      .transpose()
+      .matMul(differences)
+      .div(this.features.shape[0]);
   }
 
   train() {
