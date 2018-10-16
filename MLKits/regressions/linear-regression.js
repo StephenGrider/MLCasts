64eb354a8e1929e7fbd5c33bebbc7098133bfafd
stephen grider <ste.grider@gmail.com>
2018-10-16 12:07:05 -0600

    record mse

--- MLKits/regressions/linear-regression.js
+++ MLKits/regressions/linear-regression.js
@@ -5,6 +5,7 @@ class LinearRegression {
   constructor(features, labels, options) {
     this.features = this.processFeatures(features);
     this.labels = tf.tensor(labels);
+    this.mseHistory = [];
 
     this.options = Object.assign(
       { learningRate: 0.1, iterations: 1000 },
@@ -29,6 +30,7 @@ class LinearRegression {
   train() {
     for (let i = 0; i < this.options.iterations; i++) {
       this.gradientDescent();
+      this.recordMSE();
     }
   }
 
@@ -72,6 +74,18 @@ class LinearRegression {
     this.variance = variance;
 
     return features.sub(mean).div(variance.pow(0.5));
+  }
+
+  recordMSE() {
+    const mse = this.features
+      .matMul(this.weights)
+      .sub(this.labels)
+      .pow(2)
+      .sum()
+      .div(this.features.shape[0])
+      .get();
+
+    this.mseHistory.push(mse);
   }
 }
 
