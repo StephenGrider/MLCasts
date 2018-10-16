a8c5141277c262825b100e25af81d834c2d9b773
stephen grider <ste.grider@gmail.com>
2018-10-16 13:38:44 -0600

    plot

--- MLKits/regressions/index.js
+++ MLKits/regressions/index.js
@@ -2,6 +2,7 @@ require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./load-csv');
 const LinearRegression = require('./linear-regression');
+const plot = require('node-remote-plot');
 
 let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
   shuffle: true,
@@ -11,12 +12,17 @@ let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
 });
 
 const regression = new LinearRegression(features, labels, {
-  learningRate: 10,
+  learningRate: 0.1,
   iterations: 100
 });
 
 regression.train();
 const r2 = regression.test(testFeatures, testLabels);
 
-console.log('MSE History', regression.mseHistory);
+plot({
+  x: regression.mseHistory.reverse(),
+  xLabel: 'Iteration #',
+  yLabel: 'Mean Squared Error'
+});
+
 console.log('R2 is', r2);
