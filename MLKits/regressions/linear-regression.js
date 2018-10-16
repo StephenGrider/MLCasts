cc3893b29257793d5c274d9fa8f3bfe911bc97cb
stephen grider <ste.grider@gmail.com>
2018-10-16 14:21:39 -0600

    moving towards batch

--- MLKits/regressions/linear-regression.js
+++ MLKits/regressions/linear-regression.js
@@ -15,14 +15,14 @@ class LinearRegression {
     this.weights = tf.zeros([this.features.shape[1], 1]);
   }
 
-  gradientDescent() {
-    const currentGuesses = this.features.matMul(this.weights);
-    const differences = currentGuesses.sub(this.labels);
+  gradientDescent(features, labels) {
+    const currentGuesses = features.matMul(this.weights);
+    const differences = currentGuesses.sub(labels);
 
-    const slopes = this.features
+    const slopes = features
       .transpose()
       .matMul(differences)
-      .div(this.features.shape[0]);
+      .div(features.shape[0]);
 
     this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
   }
