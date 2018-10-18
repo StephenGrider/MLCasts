c1f9147cf1386969f58ae2f7d8e79d9101fd15d5
stephen grider <ste.grider@gmail.com>
2018-10-18 12:24:38 -0600

    Restructure

--- MLKits/regressions/linear-regression/index.js
+++ MLKits/regressions/linear-regression/index.js
@@ -1,15 +1,18 @@
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
-const loadCSV = require('./load-csv');
+const loadCSV = require('../load-csv');
 const LinearRegression = require('./linear-regression');
 const plot = require('node-remote-plot');
 
-let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
-  shuffle: true,
-  splitTest: 50,
-  dataColumns: ['horsepower', 'weight', 'displacement'],
-  labelColumns: ['mpg']
-});
+let { features, labels, testFeatures, testLabels } = loadCSV(
+  '../data/cars.csv',
+  {
+    shuffle: true,
+    splitTest: 50,
+    dataColumns: ['horsepower', 'weight', 'displacement'],
+    labelColumns: ['mpg']
+  }
+);
 
 const regression = new LinearRegression(features, labels, {
   learningRate: 0.1,
