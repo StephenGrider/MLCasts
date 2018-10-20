7d031d33deec3a2e3daaa79a3fb266fb172d9f91
stephen grider <ste.grider@gmail.com>
2018-10-19 18:33:30 -0600

    rev

--- MLKits/regressions/logistic-regression/logistic-regression.js
+++ MLKits/regressions/logistic-regression/logistic-regression.js
@@ -8,7 +8,7 @@ class LogisticRegression {
     this.costHistory = [];
 
     this.options = Object.assign(
-      { learningRate: 0.1, iterations: 1000 },
+      { learningRate: 0.1, iterations: 1000, decisionBoundary: 0.5 },
       options
     );
 
@@ -54,11 +54,13 @@ class LogisticRegression {
   predict(observations) {
     return this.processFeatures(observations)
       .matMul(this.weights)
-      .sigmoid();
+      .sigmoid()
+      .greater(this.options.decisionBoundary)
+      .cast('float32');
   }
 
   test(testFeatures, testLabels) {
-    const predictions = this.predict(testFeatures).round();
+    const predictions = this.predict(testFeatures);
     testLabels = tf.tensor(testLabels);
 
     const incorrect = predictions
