51e464b9a9e1d85d12806ebb742d47176461189a
stephen grider <ste.grider@gmail.com>
2018-10-23 15:02:11 -0600

    fix variance

--- MLKits/regressions/multinominal-logistic-regression/logistic-regression.js
+++ MLKits/regressions/multinominal-logistic-regression/logistic-regression.js
@@ -87,10 +87,15 @@ class LogisticRegression {
   standardize(features) {
     const { mean, variance } = tf.moments(features, 0);
 
-    this.mean = mean;
-    this.variance = variance;
+    const filler = variance
+      .cast('bool')
+      .logicalNot()
+      .cast('float32');
 
-    return features.sub(mean).div(variance.pow(0.5));
+    this.mean = mean;
+    this.variance = variance.add(filler);
+
+    return features.sub(mean).div(this.variance.pow(0.5));
   }
 
   recordCost() {
