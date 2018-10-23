92d3582b842e752e55d76dbc67d9f5c908efdf57
stephen grider <ste.grider@gmail.com>
2018-10-23 12:21:02 -0600

    flatten features

--- MLKits/regressions/multinominal-logistic-regression/index.js
+++ MLKits/regressions/multinominal-logistic-regression/index.js
@@ -1,39 +1,11 @@
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
-const loadCSV = require('../load-csv');
 const LogisticRegression = require('./logistic-regression');
 const plot = require('node-remote-plot');
 const _ = require('lodash');
+const mnist = require('mnist-data');
 
-const { features, labels, testFeatures, testLabels } = loadCSV(
-  '../data/cars.csv',
-  {
-    dataColumns: ['horsepower', 'displacement', 'weight'],
-    labelColumns: ['mpg'],
-    shuffle: true,
-    splitTest: 50,
-    converters: {
-      mpg: value => {
-        const mpg = parseFloat(value);
+const mnistData = mnist.training(0, 10);
 
-        if (mpg < 15) {
-          return [1, 0, 0];
-        } else if (mpg < 30) {
-          return [0, 1, 0];
-        } else {
-          return [0, 0, 1];
-        }
-      }
-    }
-  }
-);
-
-const regression = new LogisticRegression(features, _.flatMap(labels), {
-  learningRate: 0.5,
-  iterations: 100,
-  batchSize: 10
-});
-
-regression.train();
-
-console.log(regression.test(testFeatures, _.flatMap(testLabels)));
+const features = mnistData.images.values.map(image => _.flatMap(image));
+console.log(features);
