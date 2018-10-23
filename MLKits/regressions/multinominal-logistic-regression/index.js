36b40d3c77940c6df74427eeb0387cb3a3ce9ace
stephen grider <ste.grider@gmail.com>
2018-10-23 13:14:22 -0600

    implement

--- MLKits/regressions/multinominal-logistic-regression/index.js
+++ MLKits/regressions/multinominal-logistic-regression/index.js
@@ -5,7 +5,7 @@ const plot = require('node-remote-plot');
 const _ = require('lodash');
 const mnist = require('mnist-data');
 
-const mnistData = mnist.training(0, 10);
+const mnistData = mnist.training(0, 1000);
 
 const features = mnistData.images.values.map(image => _.flatMap(image));
 const encodedLabels = mnistData.labels.values.map(label => {
@@ -14,4 +14,21 @@ const encodedLabels = mnistData.labels.values.map(label => {
   return row;
 });
 
-console.log(encodedLabels);
+const regression = new LogisticRegression(features, encodedLabels, {
+  learningRate: 1,
+  iterations: 10,
+  batchSize: 100
+});
+
+regression.train();
+
+const testMnistData = mnist.testing(0, 100);
+const testFeatures = testMnistData.images.values.map(image => _.flatMap(image));
+const testEncodedLabels = testMnistData.labels.values.map(label => {
+  const row = new Array(10).fill(0);
+  row[label] = 1;
+  return row;
+});
+
+const accuracy = regression.test(testFeatures, testEncodedLabels);
+console.log('Accuracy is', accuracy);
