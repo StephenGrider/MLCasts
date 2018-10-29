59fdd0426912e26cc2bea9a825ddc31985782625
stephen grider <ste.grider@gmail.com>
2018-10-29 13:36:02 -0600

    extract data

--- loadcsv/load-csv.js
+++ loadcsv/load-csv.js
@@ -1,7 +1,19 @@
 const fs = require('fs');
 const _ = require('lodash');
 
-function loadCSV(filename, { converters = {} }) {
+function extractColumns(data, columnNames) {
+  const headers = _.first(data);
+
+  const indexes = _.map(columnNames, column => headers.indexOf(column));
+  const extracted = _.map(data, row => _.pullAt(row, indexes));
+
+  return extracted;
+}
+
+function loadCSV(
+  filename,
+  { converters = {}, dataColumns = [], labelColumns = [] }
+) {
   let data = fs.readFileSync(filename, { encoding: 'utf-8' });
   data = data.split('\n').map(row => row.split(','));
   data = data.map(row => _.dropRightWhile(row, val => val === ''));
@@ -23,10 +35,18 @@ function loadCSV(filename, { converters = {} }) {
     });
   });
 
-  console.log(data);
+  let labels = extractColumns(data, labelColumns);
+  data = extractColumns(data, dataColumns);
+
+  data.shift();
+  labels.shift();
+
+  console.log(labels);
 }
 
 loadCSV('data.csv', {
+  dataColumns: ['height', 'value'],
+  labelColumns: ['passed'],
   converters: {
     passed: val => (val === 'TRUE' ? 1 : 0)
   }
