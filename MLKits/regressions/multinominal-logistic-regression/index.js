fa0485ff89bb44781880efde39134a2f85c75cc9
stephen grider <ste.grider@gmail.com>
2018-10-29 12:06:01 -0600

    accuracy

--- MLKits/regressions/multinominal-logistic-regression/index.js
+++ MLKits/regressions/multinominal-logistic-regression/index.js
@@ -22,13 +22,13 @@ const { features, labels } = loadData();
 
 const regression = new LogisticRegression(features, labels, {
   learningRate: 1,
-  iterations: 20,
-  batchSize: 100
+  iterations: 40,
+  batchSize: 500
 });
 
 regression.train();
-debugger;
-const testMnistData = mnist.testing(0, 1000);
+
+const testMnistData = mnist.testing(0, 10000);
 const testFeatures = testMnistData.images.values.map(image => _.flatMap(image));
 const testEncodedLabels = testMnistData.labels.values.map(label => {
   const row = new Array(10).fill(0);
@@ -38,3 +38,7 @@ const testEncodedLabels = testMnistData.labels.values.map(label => {
 
 const accuracy = regression.test(testFeatures, testEncodedLabels);
 console.log('Accuracy is', accuracy);
+
+plot({
+  x: regression.costHistory.reverse()
+});
--- MLKits/regressions/multinominal-logistic-regression/logistic-regression.js
+++ MLKits/regressions/multinominal-logistic-regression/logistic-regression.js
@@ -107,7 +107,7 @@ class LogisticRegression {
     const cost = tf.tidy(() => {
       const guesses = this.features.matMul(this.weights).sigmoid();
 
-      const termOne = this.labels.transpose().matMul(guesses.log());
+      const termOne = this.labels.transpose().matMul(guesses.add(1e-7).log());
 
       const termTwo = this.labels
         .mul(-1)
@@ -117,6 +117,7 @@ class LogisticRegression {
           guesses
             .mul(-1)
             .add(1)
+            .add(1e-7)
             .log()
         );
 
